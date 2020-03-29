bdf43c70e49248fe199b579907bbc163805b3d0a
venkateshr407 <venkateshr590@gmail.com>
2020-03-29 18:40:15 +0530

    modified-8 commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -103,7 +103,7 @@ at a professional level.</h3></div>
                                 <ul className="listed">
                                     <li><p>Participated in <span className="inline">#Hacktoberfest 2019</span> and won a <span className="inline">swag's</span> for submitting four pull request's and contributing to open source <span className="inline">github</span> project's.</p></li>
                                     <li><p>Workshop on <span className="inline">Google Cloud study jam (GCP Essential) </span>at RNSIT, Bangalore.</p> </li>
-                                    <li><p>Landscape photo is commented by <span className="inline">BBCEarth </span>socal media.</p></li>
+                                    <li><p>Landscape photo is commented by <span className="inline">BBCEarth </span>social media.</p></li>
                                 </ul>
                         </section>
                     </div>
