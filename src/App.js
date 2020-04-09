4e7dacda4f6e27fc86d23004d3826b94f34cfc98
venkateshr407 <venkateshr590@gmail.com>
2020-04-09 14:06:30 +0530

    modified-images commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import {Layout} from './Layout';
 import Nav1 from './Components/Nav1';
+import Contacthit from './Components/Contacthit';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
@@ -21,18 +22,20 @@ class App extends Component{
               <Nav1/>
                 <div>
                   <Route exact path="/" component={Home} />
+                  <Route exact path="/" component={Contacthit} />
                 </div>
+      
           <div>
-             
                <Route exact path="/gallery" component={Jumbotron} />  
                <Route exact path="/gallery" component={Infotext} />
                <Route exact path="/gallery" component={Cards} />
                <Route exact path="/gallery" component={Heading} />
                <Route exact path="/gallery" component={StickyFooter1} />
-
+               <Route exact path="/gallery" component={Contacthit} />
 
                <Route path="/about" component={About}/>
               <Route path="/about" component={Layout} /> 
+              <Route exact path="/about" component={Contacthit} />
               <Route path="/about" component={StickyFooter1} />           
             </div>
          </Router>
---
+++ src/Components/Contacthit.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Contacthit extends Component {
+    render() {
+        return (
+            <div className="contact-containter">
+                <div className="hitmeup">
+                    <h1 className="hittext">HIT ME UP...</h1>
+                    <div className="media">
+                <ul>
+                    <li><a href="https://instagram.com/venky__creations/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/venki2venky1"><i class="fa fa-twitter" ria-hidden="true"></i></a></li>
+                    <li><a href="https://github.com/venkateshr407"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/venkatesh-r-0571a4178"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+            <div className="hitmail">
+            <p><a href="mailto:venkateshr590@gmail.com">  venkateshr590@gmail.com</a>SAY HI 👋</p></div>
+             
+            </div>
+                </div>
+        )
+    }
+}
